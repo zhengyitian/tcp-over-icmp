b162a7f5c5cec3b66ca4a71b4941b11217fb002c
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2019-06-12 19:46:12 +0800

    Create tcpS.py

---
+++ tcpS.py
@@ -0,0 +1,12 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+s.connect(('144.202.17.72', 9999))
+
+print(s.recv(1024).decode('utf-8'))
+for data in [b'Michael', b'Tracy', b'Sarah']:
+
+    s.send(data)
+    print(s.recv(1024).decode('utf-8'))
+s.send(b'exit')
+s.close()
