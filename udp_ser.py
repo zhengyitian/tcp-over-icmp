73b3353f5aeaeed9f80c2599b06bc0993d8ce0a1
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2019-06-12 15:58:49 +0800

    Create udp_ser.py

---
+++ udp_ser.py
@@ -0,0 +1,8 @@
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.bind(('0.0.0.0', 9999))
+print('Bind UDP on 9999...')
+while True:
+    data, addr = s.recvfrom(1024)
+    print('Received from %s:%s.' % addr)
+    s.sendto(b'Hello, %s!' % data, addr)
