a79999501ecc8a5b671fab35b1199e2a2b2a4359
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2019-06-12 19:49:05 +0800

    Update tcpS.py

--- tcpS.py
+++ tcpS.py
@@ -1,12 +1,23 @@
-import socket
+import socket,time,threading
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-s.connect(('144.202.17.72', 9999))
-
-print(s.recv(1024).decode('utf-8'))
-for data in [b'Michael', b'Tracy', b'Sarah']:
-
-    s.send(data)
-    print(s.recv(1024).decode('utf-8'))
-s.send(b'exit')
-s.close()
+s.bind(('144.202.17.72', 9999))
+s.listen(5)
+print('Waiting for connection...')
+def tcplink(sock, addr):
+    print('Accept new connection from %s:%s...' % addr)
+    sock.send(b'Welcome!')
+    while True:
+        data = sock.recv(1024)
+        time.sleep(1)
+        if not data or data.decode('utf-8') == 'exit':
+            break
+        sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+    sock.close()
+    print('Connection from %s:%s closed.' % addr)    
+while True:
+ 
+    sock, addr = s.accept()
+  
+    t = threading.Thread(target=tcplink, args=(sock, addr))
+    t.start()
+   
