c9d69eb653b60e36fdc9a9fa9bc5dbf697879fa0
zhengyitian <37394193+zhengyitian@users.noreply.github.com>
2019-06-12 17:50:39 +0800

    Update udp_ser.py

--- udp_ser.py
+++ udp_ser.py
@@ -1,8 +1,19 @@
-import socket
+import socket,json
+def mf():
+    f = open('/root/a.txt','wb')
+    f.write('a'*1024*1024*100)
+    f.close()
+mf()    
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-s.bind(('0.0.0.0', 9999))
+s.bind(('127.0.0.1', 9999))
 print('Bind UDP on 9999...')
 while True:
     data, addr = s.recvfrom(1024)
-    print('Received from %s:%s.' % addr)
-    s.sendto(b'Hello, %s!' % data, addr)
+    m = json.loads(data)
+    sta = m['s']
+    l = m['l']
+    f = open('/root/a.txt','rb')
+    f.seek(sta)
+    s2 = f.read(l)
+    f.close()   
+    s.sendto(s2, addr)
